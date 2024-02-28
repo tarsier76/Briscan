e22c2136367a9cb9761eb301328388fe557674af
yoohello <100511410+yoohello@users.noreply.github.com>
2024-02-28 09:58:59 -0500

    Added scan result messages

--- briscan.sh
+++ briscan.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-red_color="\e[31m\e[0m"
-yellow_color="\e[93m\e[0m"
-gray_color="\e[37m\e[0m"
+# Colors and scan result messages
+
+red_color="\e[31m"
+yellow_color="\e[93m"
+gray_color="\e[37m"
+green_color="\e[32m"
+no_color="\e[0m"
+
+result_good="[ ${green_color}GOOD${no_color} ]"
+result_suggestion="[ ${gray_color}SUGGESTION${no_color} ]"
+result_warning="[ ${yellow_color}WARNING${no_color} ]"
+result_alert="[ ${red_color}ALERT${no_color} ]"
