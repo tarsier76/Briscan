b5ccabfa83fddf123e867110e3c6da844dd5602d
yoohello <100511410+yoohello@users.noreply.github.com>
2024-02-28 14:32:15 -0500

    Added function for network connections analysis

--- briscan.sh
+++ briscan.sh
@@ -6,9 +6,16 @@ red_color="\e[31m"
 yellow_color="\e[93m"
 gray_color="\e[37m"
 green_color="\e[32m"
+dark_magenta_color="\e[35m"
 no_color="\e[0m"
 
 result_good="[ ${green_color}GOOD${no_color} ]"
 result_suggestion="[ ${gray_color}SUGGESTION${no_color} ]"
 result_warning="[ ${yellow_color}WARNING${no_color} ]"
 result_alert="[ ${red_color}ALERT${no_color} ]"
+
+check_suspicious_network_connections() {
+	suspected_port=$(netstat -tulnap 2>/dev/null | awk '{print $4, $5, $7}')
+}
+
+check_suspicious_network_connections
