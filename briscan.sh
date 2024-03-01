563788293d062d7847c4fecd77b7b38ae1244726
yoohello <100511410+yoohello@users.noreply.github.com>
2024-03-01 13:41:14 -0500

    Completed network connection scanning function

--- briscan.sh
+++ briscan.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-# Colors and scan result messages
+# Colors, styling and scan result messages
 
 red_color="\e[31m"
 yellow_color="\e[93m"
 gray_color="\e[37m"
 green_color="\e[32m"
 dark_magenta_color="\e[35m"
-no_color="\e[0m"
+end_style="\e[0m"
 
-result_good="[ ${green_color}GOOD${no_color} ]"
-result_suggestion="[ ${gray_color}SUGGESTION${no_color} ]"
-result_warning="[ ${yellow_color}WARNING${no_color} ]"
-result_alert="[ ${red_color}ALERT${no_color} ]"
+bold_text="\e[1m"
+
+result_good="[ ${green_color}GOOD${end_style} ]"
+result_suggestion="[ ${gray_color}SUGGESTION${end_style} ]"
+result_warning="[ ${yellow_color}WARNING${end_style} ]"
+result_alert="[ ${red_color}ALERT${end_style} ]"
+
+# Scanning info messages
+
+printf "${bold_text}Scanning your system...${end_style}\n"
+looking_for_message=$(printf "Looking for")
+
+# Functions
 
 check_suspicious_network_connections() {
-	outbound_connections_ports=$(netstat -tulnap 2>/dev/null | awk -F: '{print $3}' | cut -d " " -f 1 | awk NF)
-	outbound_connections_array=($outbound_connections_ports)
+	printf "${looking_for_message} suspicious network connections...\n"
 
-	inbound_connections_ports=$(netstat -tulnap 2>/dev/null | awk -F: '{print $2}' | cut -d " " -f 1 | awk NF)
-	inbound_connections_array=($inbound_connections_ports)
+	outbound_connections_ports=$(netstat -tulnap 2>/dev/null | awk -F: '{print $3}' | cut -d " " -f 1 | awk NF)
+	outbound_connections_pid=$(netstat -tulnap 2>/dev/null | awk '{print $7,$8}' | grep -E '[0-9]+')
+	outbound_connections_array=($outbound_connections_ports)
 
 	known_services_ports=$(cat /etc/services | awk '{print $2}' | grep -oE '[0-9]+')
 	known_services_array=($known_services_ports)
@@ -34,9 +43,11 @@ check_suspicious_network_connections() {
 		done
 
 		if [ "$port_found" == false ]; then
-			printf "Port $outbound_port might be dangerous!\n"
+			printf "The network connection using port "$outbound_port" seems dangerous!${result_warning}\n \
+        You can kill the process "${red_color}$(ps -p ${outbound_connections_pid%/*} | awk '{print $4}' | sed -n '2p')${end_style}" \
+        with PID of "${outbound_connections_pid%/*}" using 'kill -9 <PID>'.${result_suggestion}\n"
 		else
-			printf "No suspicious connections!\n"
+			printf "No suspicious network connection found for outbound port ${outbound_port}. ${result_good}\n"
 		fi
 	done
 }
