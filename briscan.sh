a868496551c266c8f615f86c67c5abe33a14db91
yoohello <100511410+yoohello@users.noreply.github.com>
2024-03-01 11:58:12 -0500

    Added check network connections function

--- briscan.sh
+++ briscan.sh
@@ -16,7 +16,29 @@ result_alert="[ ${red_color}ALERT${no_color} ]"
 
 check_suspicious_network_connections() {
 	outbound_connections_ports=$(netstat -tulnap 2>/dev/null | awk -F: '{print $3}' | cut -d " " -f 1 | awk NF)
+	outbound_connections_array=($outbound_connections_ports)
+
 	inbound_connections_ports=$(netstat -tulnap 2>/dev/null | awk -F: '{print $2}' | cut -d " " -f 1 | awk NF)
+	inbound_connections_array=($inbound_connections_ports)
+
+	known_services_ports=$(cat /etc/services | awk '{print $2}' | grep -oE '[0-9]+')
+	known_services_array=($known_services_ports)
+
+	for outbound_port in "${outbound_connections_array[@]}"; do
+		port_found=false
+		for known_port in "${known_services_array[@]}"; do
+			if [ "$outbound_port" == "$known_port" ]; then
+				port_found=true
+				break
+			fi
+		done
+
+		if [ "$port_found" == false ]; then
+			printf "Port $outbound_port might be dangerous!\n"
+		else
+			printf "No suspicious connections!\n"
+		fi
+	done
 }
 
 check_suspicious_network_connections
